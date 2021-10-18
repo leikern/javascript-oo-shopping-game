495901ee56285c9a3e2502873644acb9fa97414f
Leik Isdal <isdal.leik@gmail.com>
2021-10-18 13:48:07 +0200

    'module4'

--- js/shoppinggame.js
+++ js/shoppinggame.js
@@ -255,7 +255,7 @@ const findPointsForExpDate = (prod) => {
 const calculatePoints = (prod, tBill) => {
     let pointsToBill = findPointsToBill(Math.round(tBill));
     let pointsForExpDate = findPointsForExpDate(prod);
-    player.score = player.score + pointsForExpDate + pointsToBill;
+    player.score = player.score + pointsToBill + pointsForExpDate;
     if (prod instanceof MagicProduct) {
         if (prod.isBonus) {
             player.addPoints(prod.points);
