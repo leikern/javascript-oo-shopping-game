f4f3d74c8a9f2c7f0bf045e612ae41a15a33189e
Leik Isdal <isdal.leik@gmail.com>
2021-10-18 10:19:23 +0200

    module2

--- js/shoppinggame.js
+++ js/shoppinggame.js
@@ -66,7 +66,7 @@ const loadProducts = (map, prodId) => {
     let a = new Array();
     try {
         // Call Object.keys() to load the property names of the Product object in to prodKeys array here
-        let prodKeys = Object.keys(Product);
+        let prodKeys = Object.keys(new Product());
 
         let iterator_obj = map.entries();
 
@@ -110,7 +110,7 @@ const loadMagicProducts = (map, prodId) => {
     let a = new Array();
     try {
         // Call Object.key() to load the property names of the MagicProduct object in to magProdKeys array here
-        let magProdKeys = Object.keys(MagicProduct);
+        let magProdKeys = Object.keys(new MagicProduct());
 
         let iterator_obj = map.entries();
 
